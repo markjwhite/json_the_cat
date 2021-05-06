f046831ae5d72bfe7214edfda1826d65292ac5e9
markjwhite <markwhite259@gmail.com>
2021-05-06 14:58:24 -0400

    added mocha chai testing and fixed bugs in breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,21 +2,20 @@ const request = require('request');
 const fetchBreedDescription = (breed, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=` + breed, (error, response, body) => {
     if (error) {
-      callback(error);
-      process.exit;
+      callback(error, null);
     }
     if (response.statusCode !== 200) {
       error = (`statusCode: ${response} ${response.statusCode}`);
-      callback(error);
-      process.exit;
+      callback(error, null);
     }
     const data = JSON.parse(body);
     if (!data[0]) {
       error = ('Failed to find breed.');
-      callback(error);
-      process.exit;
+      callback(error, null);
+    } else {
+      callback(null, data[0].description);
     }
-    callback(error, data[0].description);
+
   });
 };
 
